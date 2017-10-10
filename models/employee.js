259570a3ab7e2c7b2bcdc82c9280494f9b92f592
David Regan <dregan1@comcast.net>
2017-10-10 16:07:00 -0500

    data jsons added

---
+++ models/employee.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var Employee = sequelize.define('Employee', {
+    First_Name: DataTypes.STRING,
+    Last_Name: DataTypes.STRING,
+    Department: DataTypes.STRING,
+  }, {timestamps: false});
+
+  Employee.associate = function(models) {
+    Employee.belongsTo(models.Department, {
+     foreignKey: 'departmentId',
+      onDelete: 'CASCADE'
+   });
+
+  };
+  return Employee;
+};
