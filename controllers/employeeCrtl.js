de0e5f7eb7600993b7f8e0cce8ca68122a91985e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-10-11 09:25:03 -0500

    complete edit employee function in employeeCtrl

--- controllers/employeeCrtl.js
+++ controllers/employeeCrtl.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 module.exports.getEmployee = (req, res, next) => {
   const { Employee } = req.app.get('models');
@@ -22,7 +22,14 @@ module.exports.getSingleEmployee = (req, res, next) => {
   });
 };
 
+// Bobby: HR should be able to edit an employee
 module.exports.editSingleEmployee = (req, res, next) => {
   const { Employee } = req.app.get('models');
-  Employee.update() //built-in sequelize method for editing 
+  Employee.update() //built-in sequelize method for editing
+  .then ( (employee) => {
+    res.render('index', {employee});
+  })
+  .catch( (err) => {
+    next(err); // Ship this nastyness off to our error handler at the bottom of the middleware stack in app.js
+  })
 }
