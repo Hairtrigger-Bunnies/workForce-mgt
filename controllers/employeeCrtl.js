951f26f05668359797af447b7e0f7519f2d8088e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-10-11 09:23:01 -0500

    work on task 4- editing employees

--- controllers/employeeCrtl.js
+++ controllers/employeeCrtl.js
@@ -21,3 +21,8 @@ module.exports.getSingleEmployee = (req, res, next) => {
     next(err); //Ship this nastyness off to our error handler at the bottom of the middleware stack in app.js
   });
 };
+
+module.exports.editSingleEmployee = (req, res, next) => {
+  const { Employee } = req.app.get('models');
+  Employee.update() //built-in sequelize method for editing 
+}
