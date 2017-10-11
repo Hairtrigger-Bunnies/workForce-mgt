64a5430b4552cd637c8fb5d9e21fe9aa51b2ff57
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-10-11 09:44:54 -0500

    work on adding edit functionality to the edit employee function in employeeCtrl

--- controllers/employeeCrtl.js
+++ controllers/employeeCrtl.js
@@ -25,7 +25,8 @@ module.exports.getSingleEmployee = (req, res, next) => {
 // Bobby: HR should be able to edit an employee
 module.exports.editSingleEmployee = (req, res, next) => {
   const { Employee } = req.app.get('models');
-  Employee.update() //built-in sequelize method for editing
+  //built-in sequelize method for editing 
+  Employee.update({first_name: "", last_name: "", department: "", job_title: "", supervisor: ""})
   .then ( (employee) => {
     res.render('index', {employee});
   })
@@ -33,3 +34,10 @@ module.exports.editSingleEmployee = (req, res, next) => {
     next(err); // Ship this nastyness off to our error handler at the bottom of the middleware stack in app.js
   })
 }
+
+// The equivalent call using update looks like this:
+task.update({ title: 'foooo', description: 'baaaaaar'}, {fields: ['title']}).then(() => {
+  // title will now be 'foooo' but description is the very same as before
+ })
+
+[{"First_Name":"Stanwood","Last_Name":"Muglestone","department":"Sales","created_at":"8/24/2017","updated_at":"8/28/2017","job_title":"Administrative Assistant I","supervisor ":false}];
--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -4,8 +4,13 @@ const { Router } = require('express');
 const router = Router();
 
 const { getEmployee } = require('../controllers/employeeCtrl');
+const { getSingleEmployee } = require('../controllers/employeeCtrl');
+const { postEmployee } = require('../controllers/employeeCtrl');
+const { editSingleEmployee } = require('../controllers/employeeCtrl');
 
 // When the request is a GET on the computers route, call get Computers
 router.get('/employee', getEmployee);
-
+router.get('/employee', getSingleEmployee);
+router.post('/employee', postEmployee);
+router.put('/employee', editSingleEmployee);
 module.exports = router;
