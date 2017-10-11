5ed1ad78751bb142ccbcecdf6f930920e2e12618
David Regan <dregan1@comcast.net>
2017-10-11 10:39:48 -0500

    employee gtet post

--- controllers/employeeCrtl.js
+++ controllers/employeeCrtl.js
@@ -12,12 +12,25 @@ module.exports.getEmployee = (req, res, next) => {
 };
 
 module.exports.getSingleEmployee = (req, res, next) => {
-  const { Employee } = req.app.get('models');
-  Employee.findOne() // love those built-in Sequelize methods
+  const { Employee } = req.app.get('models/:id');
+  Employee.findOne({raw: true, where: {id: req.params.id}, include: [{model: employee}] }) // love those built-in Sequelize methods
   .then( (employee) => {
+    console.log('employee', employee);
     res.render('index', {employee});
   })
   .catch( (err) => {
     next(err); //Ship this nastyness off to our error handler at the bottom of the middleware stack in app.js
   });
 };
+
+module.exports.postEmployee = (req, res, next) => {
+  const { Employee } = req.app.put('model');
+  Employee.create()
+  .then( (employee) => {
+    res.render('index', {employee});
+  })
+  .catch( (err) => {
+    next(err);
+  });
+};
+
