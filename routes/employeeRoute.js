a35f1c02d9fca59da47436c50ef09724b1a39483
David Regan <dregan1@comcast.net>
2017-10-12 15:04:41 -0500

    get employee

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -3,12 +3,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getEmployee, getSingleEmployee, postEmployee } = require('../controllers/employeeCtrl');
+const { getEmployee } = require('../controllers/employeeCtrl');
 
 
 // When the request is a GET on the computers route, call get Computers
 router.get('/employee', getEmployee);
-router.get('/employee:id', getSingleEmployee);
-router.put('/employee', postEmployee);
+// router.get('/employee:id', getSingleEmployee);
+// router.put('/employee', postEmployee);
 
 module.exports = router;
