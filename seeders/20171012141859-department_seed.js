02d7ea7286d61fd654a1ac115e46e8170be5d012
David Regan <dregan1@comcast.net>
2017-10-12 13:51:28 -0500

    fixed issues to use nodemon

---
+++ seeders/20171012141859-department_seed.js
@@ -0,0 +1,19 @@
+'use strict';
+let { departments } = require('./data/department.json')
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return queryInterface.bulkInsert('Departments', departments, {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    return queryInterface.bulkDelete('Departments', null, {});
+  }
+};
