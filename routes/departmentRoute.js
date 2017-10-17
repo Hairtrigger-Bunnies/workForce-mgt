fe7839a4bea414377863beed193534045c0a81fe
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-10-17 13:20:30 -0500

    work on department details so user can see which employees belong to whichever department

--- routes/departmentRoute.js
+++ routes/departmentRoute.js
@@ -3,12 +3,11 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getDepartment, postDepartment, editDepartment } = require('../controllers/departmentCtrl');
+const { getDepartment, postDepartment, editDepartment, getDepartmentDetails } = require('../controllers/departmentCtrl');
 
 // When the request is a GET on the computers route, call get Computers
 router.get('/department', getDepartment);
 router.post('/department', postDepartment);
 router.put('/department/:id', editDepartment);
-// router.get('./departmentDetails', getdepartmentDetails);
 
 module.exports = router;
